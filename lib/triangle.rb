8a7736efd0eb712fdfc96551013ded8aa941d56f
Jeff Swisher <jefftswisher@gmail.com>
2019-11-11 20:36:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
+ attr_accessor :a, :b, :c 
+ 
+ def initialize(a,b,c)
+   @a = a 
+   @b = b 
+   @c = c 
+ end 
+ 
+ def kind(a,b,c)
+   if a == b && a ==c && b == c 
+     :equilateral
+   elsif 
+     :isoceles 
+   else a != b && a != c && b != c 
+     :scalene 
+   
+ end 
+ 
+ class TraingleError < StandardError 
+ end 
+ 
  
 end
